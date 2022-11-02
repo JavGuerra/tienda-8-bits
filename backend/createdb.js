0d78d6c9bc0d1b5da2660d8d46a120ec55622c87
JavGuerra <javguerra@badared.com>
2022-11-02 02:26:39 +0100

    Primer commit

--- backend/createdb.js
+++ backend/createdb.js
@@ -1,38 +1,31 @@
+// Script para llenar y relacionar las colecciones
 // Usar con: node createdb
 // Nota: La BBDD y las colecciones deben estar creadas previamente
-
 import initVars  from './src/config/vars.js';
 import connectDB from './src/config/db.js';
-
 import Product   from './src/schemas/ProductSchema.js';
 import products  from './data/products.js';
 import Manufacturer  from './src/schemas/ManufacturerSchema.js';
 import manufacturers from './data/manufacturers.js';
 
-const createdb = async () => {
+(async () => {
     const { url } = await initVars();
     const db = await connectDB(url);
 
-    console.log('Limpiando...');
-    await Product.deleteMany({});
-    await Manufacturer.deleteMany({});
+    console.log("Limpiando...");
+    await Product.deleteMany();
+    await Manufacturer.deleteMany();
 
-    console.log('Llenando las colecciones...');
+    console.log("Llenando las colecciones...");
     const newManufacturers = await Manufacturer.insertMany(manufacturers);
 
-    const newProducts = products.map( product => {
+    const newProducts = products.map(product => {
         const manufacturer = newManufacturers.filter(
             manufacturer => manufacturer.code === product.manufacturer)[0];
         const { _id, name } = manufacturer;
-        return {
-            ...product,
-            manufacturer: { ref: _id, name }
-        };
+        return { ...product, manufacturer: { ref: _id, name } }
     });
     await Product.insertMany(newProducts);
 
-    db.disconnect();
-    console.log('Conexión finalizada');
-};
-
-createdb();
+    db.close(() => console.log("Conexión finalizada"));
+})();
