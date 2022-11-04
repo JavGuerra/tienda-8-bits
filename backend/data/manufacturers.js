30be61aef2562ebd62dea724fa3a1f1069a319a9
JavGuerra <javguerra@badared.com>
2022-11-04 19:50:15 +0100

    Primer commit

--- backend/data/manufacturers.js
+++ backend/data/manufacturers.js
@@ -14,6 +14,22 @@ const manufacturers = [
         description: "Desarrolló portátiles en conjunción con Toshiba.",
         logo: "sord.png",
         link: "https://es.wikipedia.org/wiki/Sord_Computer_Corporation"
+    },
+    {
+        brand: "Texas Instruments",
+        code: "003",
+        country: "Estados Unidos",
+        description: "Conocida por sus calculadoras, electrónica de consumo y ordenadores.",
+        logo: "ti.png",
+        link: "https://es.wikipedia.org/wiki/Texas_Instruments"
+    },
+    {
+        brand: "Commodore International",
+        code: "004",
+        country: "Estados Unidos",
+        description: "Compañía pionera creadora de ordenadores de masas como el Pet o el C64.",
+        logo: "cbm.png",
+        link: "https://es.wikipedia.org/wiki/Commodore_International"
     }
 ];
 
--- backend/data/products.js
+++ backend/data/products.js
@@ -7,7 +7,7 @@ const products = [
         relevance: true,
         price: 300,
         year: 1982,
-        photo: "spectrum.jpg",
+        photo: "zx.jpg",
         link: "https://es.wikipedia.org/wiki/Sinclair_ZX_Spectrum"
     },
     {
@@ -29,8 +29,30 @@ const products = [
         relevance: false,
         price: 150,
         year: 1982,
-        photo: "sordm5.jpg",
+        photo: "m5.jpg",
         link: "https://es.wikipedia.org/wiki/Sord_M5"
+    },
+    {
+        model: "TI 99/4A",
+        code: "004",
+        manufacturer: "003",
+        info: "16Kb. Ampliable a 52Kb. Buen teclado y buen precio. BASIC lento.",
+        relevance: false,
+        price: 250,
+        year: 1984,
+        photo: "ti99.jpg",
+        link: "https://es.wikipedia.org/wiki/Texas_Instruments_TI-99/4A"
+    },
+    {
+        model: "Commodore 64",
+        code: "005",
+        manufacturer: "004",
+        info: "64Kb. Buen teclado y precio. Excelentes gráficos y sonido. BASIC limitado.",
+        relevance: true,
+        price: 250,
+        year: 1982,
+        photo: "c64.jpg",
+        link: "https://es.wikipedia.org/wiki/Commodore_64"
     }
 ];
 
