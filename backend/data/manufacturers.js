165b89ad9614902df22d974db3e7fb8721d3d450
JavGuerra <javguerra@badared.com>
2022-11-11 09:48:10 +0100

    Primer commit

--- backend/data/manufacturers.js
+++ backend/data/manufacturers.js
@@ -3,7 +3,6 @@ const manufacturers = [
         brand: "Sinclair Research Ltd.",
         code: "001",
         country: "Reino Unido",
-        description: "La compañía de Sir Clive Sinclair.",
         logo: "sinclair.png",
         link: "https://es.wikipedia.org/wiki/Sinclair_Research"
     },
@@ -11,7 +10,6 @@ const manufacturers = [
         brand: "Sord Computer Corp.",
         code: "002",
         country: "Japón",
-        description: "Desarrolló portátiles en conjunción con Toshiba.",
         logo: "sord.png",
         link: "https://es.wikipedia.org/wiki/Sord_Computer_Corporation"
     },
@@ -19,7 +17,6 @@ const manufacturers = [
         brand: "Texas Instruments",
         code: "003",
         country: "Estados Unidos",
-        description: "Conocida por sus calculadoras, electrónica de consumo y ordenadores.",
         logo: "ti.png",
         link: "https://es.wikipedia.org/wiki/Texas_Instruments"
     },
@@ -27,7 +24,6 @@ const manufacturers = [
         brand: "Commodore International",
         code: "004",
         country: "Estados Unidos",
-        description: "Compañía pionera creadora de ordenadores de masas como el Pet o el C64.",
         logo: "cbm.png",
         link: "https://es.wikipedia.org/wiki/Commodore_International"
     }
--- backend/src/schemas/ManufacturerSchema.js
+++ backend/src/schemas/ManufacturerSchema.js
@@ -2,12 +2,11 @@ import { model, Schema } from 'mongoose';
 
 const manufacturerSchema = new Schema(
     {
-        brand: { type: String, uppercase: true, trim: true },
-        code:  { type: String, index: { unique: true }, trim: true},
+        brand:   { type: String, uppercase: true, trim: true },
+        code:    { type: String, index: { unique: true }, trim: true},
         country: { type: String, trim: true },
-        description: { type: String, trim: true },
-        logo:  { type: String, lowercase: true, trim: true },
-        link:  { type: String, trim: true }
+        logo:    { type: String, lowercase: true, trim: true },
+        link:    { type: String, trim: true }
     },
     { versionKey: false }
 );
