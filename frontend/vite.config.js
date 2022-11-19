a9543fb1c6ece5438c53dddf16030659b1139119
JavGuerra <javguerra@badared.com>
2022-11-19 21:33:16 +0100

    Frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "https://javguerra.badared.com/proyecto/web8bits/"
+  plugins: [react()]
 })
