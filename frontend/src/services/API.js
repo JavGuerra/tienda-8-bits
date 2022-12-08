423df4e9625fbff6cf993989321565f9b376db8f
JavGuerra <javguerra@badared.com>
2022-12-08 20:39:45 +0100

    Primer commit

--- frontend/src/services/API.js
+++ frontend/src/services/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const getData = (url) => {
-    const request = axios.get(url);
+    const  request = axios.get(url);
     return request.then(response => response.data);
 }
 
