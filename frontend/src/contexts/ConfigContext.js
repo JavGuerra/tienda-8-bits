71d466030a286da2c36ec8b332d49a4b6b0273a4
JavGuerra <javguerra@badared.com>
2022-12-09 00:45:45 +0100

    Primer commit

--- frontend/src/contexts/ConfigContext.js
+++ frontend/src/contexts/ConfigContext.js
@@ -1,9 +1,9 @@
 import { createContext } from "react";
 
 const Config = createContext({
-  title:   'Tienda 8 bits',
-  footer:  'Desarrollado por: Javier Guerra',
-  link:    'https://javguerra.github.io/'
+  title:  'Tienda 8 bits',
+  footer: 'Desarrollado por: Javier Guerra',
+  link:   'https://javguerra.github.io/'
 });
 
 export default Config;
