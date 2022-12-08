edff6df8dfc1903f9aaebf4e7f81ce702db552d7
JavGuerra <javguerra@badared.com>
2022-12-08 10:53:23 +0100

    Primer commit

--- frontend/src/components/SortIcon.jsx
+++ frontend/src/components/SortIcon.jsx
@@ -7,9 +7,8 @@ const SortIcon = ({order, alt}) => {
       : "M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0l-3.75-3.75M17.25 21L21 17.25";
 
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" className="icon" fill="none"
-      viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
-      role="img" aria-label={alt} >
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="icon"
+      fill="none" strokeWidth={1.5} stroke="currentColor" role="img" aria-label={alt} >
       <path strokeLinecap="round" strokeLinejoin="round" d={d} />
     </svg>
   );
