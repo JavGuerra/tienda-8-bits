e73657d4a84e00d40676414445b5050a06c5f502
JavGuerra <javguerra@badared.com>
2022-12-18 02:34:40 +0100

    First commit

--- backend/src/config/express.js
+++ backend/src/config/express.js
@@ -8,30 +8,32 @@ import v1ProductRoutes from '../v1/routes/productRoutes.js';
 
 const expressApp = express();
 
-// Middlewares
+expressApp
 
-expressApp.use(cors());
-expressApp.use(compression());
-expressApp.use(helmet({ crossOriginResourcePolicy: false }));
+    // Middlewares
 
-expressApp.use((req, res, next) => {
-    console.log('Ruta: ' + req.url);
-    next();
-});
+    .use(cors())
+    .use(compression())
+    .use(helmet({ crossOriginResourcePolicy: false }))
 
-// Routes
+    .use((req, res, next) => {
+        console.log('Ruta: ' + req.url);
+        next();
+    })
 
-expressApp.use(express.static('public'));
+    // Routes
 
-expressApp.use('/v1/manufacturers', v1ManufacturerRoutes);
-expressApp.use('/v1/products', v1ProductRoutes);
+    .use(express.static('public'))
 
-expressApp.use((req, res) => res.status(404).send('Error 404: No encontrado.'));
+    .use('/v1/manufacturers', v1ManufacturerRoutes)
+    .use('/v1/products', v1ProductRoutes)
 
-expressApp.use((err, req, res, next) => {
-    console.error(err.stack);
-    const status = err.status || 500;
-    res.status(status).send(`Error ${status}: ${err.message}.`);
-});
+    .use((req, res) => res.status(404).send('Error 404: No encontrado.'))
+
+    .use((err, req, res, next) => {
+        console.error(err.stack);
+        const status = err.status || 500;
+        res.status(status).send(`Error ${status}: ${err.message}.`);
+    });
 
 export default expressApp;
