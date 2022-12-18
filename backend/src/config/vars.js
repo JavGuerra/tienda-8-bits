6dddb1a5b3ca0aa36875c471313c83f4c38f1118
JavGuerra <javguerra@badared.com>
2022-12-18 03:00:00 +0100

    First commit

--- backend/src/config/vars.js
+++ backend/src/config/vars.js
@@ -7,11 +7,10 @@ const initVars = async () => {
         dotenv.config();
     }
     
-    let url;
+    let url  = process.env.DB_PROTOCOL;
     if (process.env.DB_SERVERTYPE === 'local') {
-        url  = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/`;
+        url += `${process.env.DB_HOST}:${process.env.DB_PORT}/`;
     } else {
-        url  = `mongodb+srv://`;
         url += `${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@`;
         url += `${process.env.DB_CLUSTER}.${process.env.DB_HOST}/`
     }
